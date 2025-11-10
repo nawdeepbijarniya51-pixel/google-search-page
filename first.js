5a8e81b054c960492f3294bdfecb83ebc482bc21
nawdeepbijarniya51-pixel <nawdeepbijarniya51@gmail.com>
2025-11-10 16:15:34 +0530

    first.js

--- first.js
+++ first.js
@@ -1,17 +1,17 @@
-    
-    let addBtn = document.querySelector(".add-icon-btn");
-    let searchbox = document.querySelector("input");
-    
 
-    addBtn.addEventListener("click", () => {
-        let iconName = prompt("Name of icon:");
-        let iconUrl = prompt("Enter URL:");
-        if (!iconName || !iconUrl) return; 
+let addBtn = document.querySelector(".add-icon-btn");
+let searchbox = document.querySelector("input");
 
-        let iconBox = document.createElement("div");
-        iconBox.classList = "iconbox";
 
-        iconBox.innerHTML = `
+addBtn.addEventListener("click", () => {
+    let iconName = prompt("Name of icon:");
+    let iconUrl = prompt("Enter URL:");
+    if (!iconName || !iconUrl) return;
+
+    let iconBox = document.createElement("div");
+    iconBox.classList = "iconbox";
+
+    iconBox.innerHTML = `
             <button class='icon'>
                 <h1><img src="mic.svg" alt="${iconName}"></h1>
             </button>
@@ -19,19 +19,19 @@
         `;
 
 
-        iconBox.querySelector(".icon").addEventListener("click", () => {
-            window.location.href = iconUrl;
-        });
+    iconBox.querySelector(".icon").addEventListener("click", () => {
+        window.location.href = iconUrl;
+    });
 
 
-        document.querySelector(".toolbox").append(iconBox);
-    });
-    searchbox.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-            e.preventDefault();
-            const query = searchbox.value.trim();
-            if (!query) return;
-            const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
-            window.location.href = searchUrl; 
-        }
-    });
+    document.querySelector(".toolbox").append(iconBox);
+});
+searchbox.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        const query = searchbox.value.trim();
+        if (!query) return;
+        const searchUrl = 'https://www.google.com/search?q=' + encodeURIComponent(query);
+        window.location.href = searchUrl;
+    }
+});
